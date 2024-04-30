1d73d29c2d76d44c6e7c9820df4393e47dc7ff14
THUMMA PRASANTH REDDY <52069294+prasanth50@users.noreply.github.com>
2024-04-30 13:44:54 -0700

    Adding a comment to index.js

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get('/tshirt', (req, res) => {
         size: 'Large'
     })
 });
-
+// You can get the API listener output for the t-shirt from the following console log
 app.listen(PORT, () => console.log(`It's live on http://localhost:${PORT}`));
